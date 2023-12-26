8d9f7952c8c699ce3f424ed9e755432708c2c480
Dreamtank <>
2023-12-26 23:29:15 +0900

    2024 12 26 结构控制的分支控制学习

---
+++ 第四章控制结构/二分支控制/IfelseExercise.java
@@ -0,0 +1,34 @@
+package 控制结构.二分支控制;
+
+import java.util.Scanner;
+
+public class IfelseExercise {
+
+	public static void main(String[] args) {
+		
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.println("请输入信用分：");
+		
+		int xinyong = myScanner.nextInt();
+		
+		if(xinyong >= 1 && xinyong <= 100) {
+			if(xinyong == 100) {
+				System.out.println("信用极好");
+			} else if (xinyong > 80 && xinyong < 99) {
+				System.out.println("信用优秀");
+			} else if (xinyong > 60 && xinyong < 80) {
+				System.out.println("信用一般");
+			} else {
+				System.out.println("不及格");
+			}
+			
+		} else {
+			System.out.println("信用分需要在1-100，请重新输入：");
+		}
+		
+		
+		
+	}
+	
+}
