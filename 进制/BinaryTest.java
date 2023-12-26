20b0e88b6079537b9a86aa37f68e1e966efbbbff
Dreamtank <>
2023-12-26 17:21:19 +0900

    2023 12 26 进制学习

---
+++ 进制/BinaryTest.java
@@ -0,0 +1,18 @@
+package 进制;
+
+public class BinaryTest {
+	public static void main(String[] args) {
+		int n1 = 0b1010;
+		int n2 = 1010;
+		int n3 = 01010;
+		int n4 = 0X10101;
+		//二进制
+		System.out.println("n1 = " + n1);
+		//八进制
+		System.out.println("n2 = " + n2);
+		//十进制
+		System.out.println("n3 = " + n3);
+		//十六进制 
+		System.out.println("n4 = " + n4);
+	}
+}
